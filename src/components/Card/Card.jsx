656e28c23648b2612fcf1059179501823fb30ced
AlsuLI <142306929+AlsuLI@users.noreply.github.com>
2023-10-27 08:17:03 -0700

    added styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,16 +7,18 @@ export const Card = ({ name, actor, gender, house, core, alive, id, image, click
  
   return (
     <div className={s.card__wrapper}>
-      <input
-        onClick={clickLike}
-        type="image"
-        src={liked ? Liked : Like}
-        className={s.card__button__like}
-      />
+      <div className={s.card__button__like}>
+        <input
+          className={s.card__button__input}
+          onClick={clickLike}
+          type="image"
+          src={liked ? Liked : Like}
+        />
+      </div>
       <img src={image} alt="" className={s.card__image} />
       <div className={s.card__text__wrapper}>
         <h2 className={s.card__text__name}>Name: {name}</h2>
-        <ul  className={s.card__text__holder}>
+        <ul className={s.card__text__holder}>
           <li>Actor: {actor}</li>
           <li>Gender: {gender}</li>
           <li>House: {house}</li>
